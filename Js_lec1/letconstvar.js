1019c268e2c84ea57bf3aff5ab7b17777afd2fa9
Girija Vaidya <vaidyagirija58@gmail.com>
2025-06-28 03:44:52 -0700

    Starting Project

---
+++ Js_lec1/letconstvar.js
@@ -0,0 +1,22 @@
+const accountId = "1234543"
+let accountEmail = "girija@gmail.com"
+var accountPassword = "123454"
+accountCity ="Nagpur"
+let accountState;
+// if we do not give the value so the output get as undefined
+
+// accountId = 2
+//  do not change the constant value
+
+accountEmail ="sanket@gmail.com"
+accountPassword ="7987890"
+accountCity = "amravati"
+// do not write the variable can consiter the variable
+
+console.log(accountId);
+//Do not use var
+// prefer not to use var because of issue in block scope and functional scope
+
+// tabulate for,
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) 
+
