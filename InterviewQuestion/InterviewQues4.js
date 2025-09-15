2befef98f388fb23132900717ab5f342e5ccac12
Girija Vaidya <vaidyagirija58@gmail.com>
2025-09-15 13:04:57 +0530

    Adding new file

---
+++ InterviewQuestion/InterviewQues4.js
@@ -0,0 +1,15 @@
+var variable = 10;
+
+(() =>
+{
+    console.log(variable);
+    variable = 20;
+    
+    console.log(variable);
+    
+}
+// iife
+    )(); 
+    console.log(variable);
+    var variable = 30;
+    console.log(variable);
